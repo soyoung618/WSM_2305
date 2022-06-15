33e529b5ee88fe0ee1482fe3409d0bd0d240fd49
SSep <ssep4u@gmail.com>
2022-06-15 11:44:57 +0900

    html,css,js toggleMenu() 1

---
+++ 20220608_school cafeteria/js/main.js
@@ -0,0 +1,32 @@
+// show/hide menu: toggle menu
+function toggleMenu(toggleId, navListId) {
+  //html -> js
+  const toggle = document.getElementById(toggleId);
+  const navList = document.getElementById(navListId);
+
+  function clickHandler() {
+    //show/hide menu: .show-menu
+    navList.classList.toggle('show-menu');
+  }
+
+  if (toggle && navList) {
+    //toggle click
+    toggle.addEventListener('click', clickHandler);
+  }
+}
+toggleMenu("nav-toggle", "nav-list");
+
+// const say = () => console.log("hello world4");
+
+// const say = () => {
+//   console.log("hello world3");
+// };
+
+// const say = function () {
+//   console.log("hello world2");
+// };
+
+// function say() {
+//   console.log("hello world");
+// }
+// say();
