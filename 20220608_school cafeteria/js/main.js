330a80f2d7775f01b9c6c833304d1a287ce07d8d
SSep <ssep4u@gmail.com>
2022-06-15 12:00:51 +0900

    js change toggle icon

--- 20220608_school cafeteria/js/main.js
+++ 20220608_school cafeteria/js/main.js
@@ -3,12 +3,16 @@ const toggleMenu = (toggleId, navListId) => {
   //html -> js
   const toggle = document.getElementById(toggleId);
   const navList = document.getElementById(navListId);
+  const toggleIcon = toggle.getElementsByTagName("i")[0];
 
   if (toggle && navList) {
     //toggle click
     toggle.addEventListener("click", () => {
       //show/hide menu: .show-menu
       navList.classList.toggle("show-menu");
+      //change toggle icon: bx-menu <-> bx-x
+      toggleIcon.classList.toggle("bx-menu");
+      toggleIcon.classList.toggle("bxs-x-square");
     });
   }
 };
